924b7ef3a3224ae3ec5c284741c58eb66cd905fe
unknown <vavan7520@gmail.com>
2020-04-20 17:55:23 +0300

    добавление скрола выдиление красным телефон через библиотеку jquery

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,72 @@
-var button = document.querySelector('#button');
-var modal = document.querySelector('#modal');
-var close = document.querySelector('#close');
+// функция вывода модального окна
 
+$(document).ready(function () {
+	var button = $('#button');
+	var modal = $('#modal');
+	var close = $('#close');
 
-button.addEventListener('click', function() {
-	modal.classList.add('modal_active');
+	button.on('click', function () {
+
+		modal.addClass('modal_active')
+	});
+
+	close.on('click', function () {
+
+		modal.removeClass('modal_active')
+	});
+});
+
+// функция скролинг кнопки вверх
+
+$(document).ready(function () {
+
+	$(window).scroll(function () {
+
+		if ($(this).scrollTop() != 0) {
+
+			$('#up').fadeIn();
+
+		} else {
+
+			$('#up').fadeOut();
+
+		}
+
+	});
+
+	$('#up').click(function () {
+
+		$('body,html').animate({ scrollTop: 0 }, 500);
+
+	});
 });
 
 
-close.addEventListener('click', function() {
-	modal.classList.remove('modal_active');
+// функция доиавление нового класса для номера телефона лелает телефон при наведении красным в navbar
+
+$(document).ready(function () {
+
+	$('#read').mouseenter(function () {
+
+		$(this).toggleClass('navbar__phone-red');
+	});
+	$('#read').mouseleave(function () {
+
+		$(this).toggleClass('navbar__phone-red');
+	});
+});
+
+// функция доиавление нового класса для номера телефона лелает телефон при наведении красным в offer
+
+
+$(document).ready(function () {
+
+	$('#read2').mouseenter(function () {
+
+		$(this).toggleClass('offer__phone-red');
+	});
+	$('#read2').mouseleave(function () {
+
+		$(this).toggleClass('offer__phone-red');
+	});
 });
